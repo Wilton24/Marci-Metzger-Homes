154305c9c0226845a77979aa6960471e53c86028
Wilton Batiquin <cbasher24@gmail.com>
2026-01-13 16:17:08 +0800

    Refactor Header component and styles; add hero section with navigation and logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,38 @@
+import HeroImg from "../../assets/images/heroImg.jpg";
+import styles from "./header.module.css";
+import logoImg from "../../../public/MarciMetzgerLogo.webp"
+
 export default function Header() {
     return (
-        <header>
-            <h1>Marci Metzger Homes</h1>
-        </header>
-    )
+        <main className={styles.wrapper}>
+            <section
+                className={styles.heroSection}
+                style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url(${HeroImg})` }}
+            >
+                <header className={styles.header}>
+                    <div className={styles.logo}>
+                        {/* 🏠 */}
+                        <img src={logoImg} alt="Marci Metzger Homes Logo" />
+                    </div>
+                    <nav className={styles.nav}>
+                        <ul className={styles.navList}>
+                            <li>HOME</li>
+                            <li>LISTINGS</li>
+                            <li>LET'S MOVE</li>
+                            <li>ABOUT US</li>
+                        </ul>
+                    </nav>
+                    <div className={styles.authButtons}>
+                        <button className={styles.loginBtn}>Login</button>
+                        <button className={styles.signUpBtn}>Sign up</button>
+                    </div>
+                </header>
+
+                <div className={styles.heroContent}>
+                    <h1>Find where you belong.</h1>
+                    {/* My searchbar probably */}
+                </div>
+            </section>
+        </main>
+    );
 }
