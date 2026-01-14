5862289e6413a861731b45decd5de028b97437ba
Wilton Batiquin <cbasher24@gmail.com>
2026-01-14 16:19:29 +0800

    Enhance BodySection component with new layout and content; add contact details and consultation button

--- src/components/BodySection.tsx
+++ src/components/BodySection.tsx
@@ -1,26 +1,37 @@
 import styles from './body-section.module.css';
 import realtorImg from "../assets/realtor_Img.jfif";
-// import threeDecadesLogo from "../assets/images/threeDecadesLogo.jpg";
 import ExcellenceBadge from './UI/ExcellenceBadge';
 
 export default function Section() {
     return (
         <section className={styles.wrapper}>
+            {/* Left section */}
             <div className={styles.leftSection}>
                 <div className={styles.imageWrapper}>
                     <img
                         src={realtorImg}
                         className={styles.realtorImage}
                         alt=" An image of Marci Metzger" />
-                    <ExcellenceBadge />
+                    <div className={styles.excellenceBadgeContainer}>
+                        <ExcellenceBadge />
+                    </div>
+
                 </div>
 
             </div>
 
-
-            <div className={styles.rightSection}></div>
-            {/* <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolorum nostrum doloribus nisi optio aut error
-                repellendus omnis neque corporis voluptatibus illo harum dolore provident repudiandae iusto, ad ipsum quos!</h2> */}
+            {/* Right section */}
+            <div className={styles.rightSection}>
+                <h2 className={styles.trustedLocalExpert}>TRUSTED LOCAL EXPERT</h2>
+                <h1 className={styles.realtorName}>Marci Metzger</h1>
+                <p className={styles.description}>With over 30 years of experience, Marci Metzger has built a reputation for excellence in real estate. Her dedication
+                    to her clients and the community has made her a trusted name in the industry.</p>
+                <div className={styles.contactDetails}>
+                    <h2 className={styles.directLine}>DIRECT LINE</h2>
+                    <h1 className={styles.contactNumber}>206.919.6886</h1>
+                </div>
+                <button className={styles.bookConsultationButton}>Book a Consultation</button>
+            </div>
         </section>
     )
 }
